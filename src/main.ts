d87bae512015f6563510adb6eb9a6703ba92c41d
jowjow22 <jonathan224santos@gmail.com>
2025-03-31 12:57:59 -0300

    feat: test url sentry

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ const app = createApp(App)
 
 Sentry.init({
   app,
-  dsn: "https://a0648b689418ee1195fb094b1d49cd7e@o4509072911368192.ingest.us.sentry.io/4509072913465344",
+  dsn: import.meta.env.VUE_APP_SENTRY_DSN,
 });
 
 app.use(router).use(pinia).mount("#app");
